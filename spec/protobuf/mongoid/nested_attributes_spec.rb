6004754addb12bc8fc505a94f3a1d5fdb4576cdd
Luilver Garces <luilver.garces@mx.com>
2025-04-27 16:43:32 -0600

    Fix offenses

--- spec/protobuf/mongoid/nested_attributes_spec.rb
+++ spec/protobuf/mongoid/nested_attributes_spec.rb
@@ -1,12 +1,14 @@
-require "spec_helper"
+# frozen_string_literal: true
+
+require 'spec_helper'
 
 describe Protobuf::Mongoid::NestedAttributes do
   let(:user_message) {
-    UserMessage.new(:name => "foo bar", :email => "foo@test.co", :photos => [{ :url => "https://test.co/test.png" }])
+    UserMessage.new(name: 'foo bar', email: 'foo@test.co', photos: [{ url: 'https://test.co/test.png' }])
   }
 
-  describe "._filter_attribute_fields", :aggregate_failures => true do
-    it "includes nested attributes" do
+  describe '._filter_attribute_fields', aggregate_failures: true do
+    it 'includes nested attributes' do
       attribute_fields = User._filter_attribute_fields(user_message)
       expect(attribute_fields[:photos_attributes]).to eq(user_message.photos)
     end
@@ -14,10 +16,10 @@ describe Protobuf::Mongoid::NestedAttributes do
 
   # TODO: This test is not working as expected. It should be fixed.
   # NoMethodError:
-  #     undefined method `with_indifferent_access' for "https://test.co/test.png":String
-  describe ".new" do
-    context "when a model accepts nested attributes" do
-      xit "transforms nested attributes", :aggregate_failures => true do
+  #     undefined method `with_indifferent_access' for 'https://test.co/test.png':String
+  describe '.new' do
+    context 'when a model accepts nested attributes' do
+      xit 'transforms nested attributes', aggregate_failures: true do
         user_message.photos.each do |photo_message|
           expect(Photo).to receive(:attributes_from_proto).with(photo_message).and_call_original
         end
